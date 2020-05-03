3d24c0a196b4f8608c7bee2032bfe59659be2a13
Connor Wenck <cwenck@umd.edu>
2020-05-03 17:55:08 -0400

    Testing init

--- init.lua
+++ init.lua
@@ -1,30 +1,29 @@
-args = {...}
-
-programs = {
+local programs = {
     "init",
     "branch-miner"
 }
 
 function main()
     for name in pairs(programs) do
-        downloadProgram(name)
+        print(name)
+        -- downloadProgram(name)
     end
 end
 
-function generateUrlForProgram(name)
-    local baseUrl = "https://raw.githubusercontent.com/cwenck/cc-turtles/master/"
-    return baseUrl .. name .. ".lua"
-end
+-- function generateUrlForProgram(name)
+--     local baseUrl = "https://raw.githubusercontent.com/cwenck/cc-turtles/master/"
+--     return baseUrl .. name .. ".lua"
+-- end
 
-function downloadProgram(name)
-    local url = generateUrlForProgram(name)
+-- function downloadProgram(name)
+--     local url = generateUrlForProgram(name)
 
-    -- Delete the program if it already exists
-    if fs.exists(name) then
-        fs.delete(name)
-    end
+--     -- Delete the program if it already exists
+--     if fs.exists(name) then
+--         fs.delete(name)
+--     end
 
-    -- Download the program
-    local fullName = name .. ".lua"
-    shell.run("wget", url, fullName)
-end
+--     -- Download the program
+--     local fullName = name .. ".lua"
+--     shell.run("wget", url, fullName)
+-- end
