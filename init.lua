6f867c0f512cec59d604e595f7e545a3b83ce076
Connor Wenck <cwenck@umd.edu>
2020-05-03 17:46:04 -0400

    Added old branch mining program

--- init.lua
+++ init.lua
@@ -1,5 +1,29 @@
 args = {...}
 
+programs = {
+    "init"
+    "branch-miner"
+}
+
 function main()
-    -- Do Nothing
+    for name in pairs(programs) do
+        downloadProgram(name)
+    end
+end
+
+function generateUrlForProgram(name)
+    local baseUrl = "https://raw.githubusercontent.com/cwenck/cc-turtles/master/"
+    return baseUrl .. name .. ".lua"
+end
+
+function downloadProgram(name)
+    local url = generateUrlForProgram(name)
+
+    -- Delete the program if it already exists
+    if fs.exists(name) then
+        fs.delete(name)
+    end
+
+    -- Download the program
+    shell.run("wget", url, name)
 end
