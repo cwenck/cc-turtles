c96a26789f8d01bc1724ace831da7e06560e8718
Connor Wenck <cwenck@umd.edu>
2020-05-04 22:02:28 -0400

    Added web program to download files

---
+++ web.lua
@@ -0,0 +1,38 @@
+if not http then
+    printError("Requires the HTTP API")
+    printError("Set http_enable to true in ComputerCraft.cfg")
+    return
+end
+ 
+local function get(url)
+    local ok, err = http.checkURL(url)
+    if not ok then
+        if err then
+            printError(err)
+        end
+        return nil
+    end
+
+    local headers = {
+        ["Cache-Control"] = "no-cache"
+    }
+
+    local response = http.get(url, headers)
+    if not response then
+        return nil
+    end
+
+    local responseBody = response.readAll()
+    response.close()
+    return responseBody
+end
+ 
+function download(url, name)
+    local path = shell.resolve(name)
+    local response = get(url)
+    if response then
+        local file = fs.open(path, "wb")
+        file.write(response)
+        file.close()
+    end
+end
