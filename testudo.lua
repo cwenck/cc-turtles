a1dc51a4b9d474337ecef6c467060a40110e1b61
Connor Wenck <none@example.com>
2021-01-14 23:18:20 -0500

    Corrected not-equals syntax

--- testudo.lua
+++ testudo.lua
@@ -328,7 +328,7 @@ function countItems()
     local itemCounts = {}
 
     for _, slotInfo in pairs(inventory) do
-        if slotInfo not nil then
+        if slotInfo ~= nil then
             local name = slotInfo.details.name
             local count = slotInfo.details.count
             itemCounts[name] = count + util.getOrDefault(itemCounts[name], 0)
