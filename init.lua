d09715f3bb3187f227f2c9d410ba4cb2bf944220
Connor Wenck <none@example.com>
2021-01-14 19:50:50 -0500

    Attempt #3 to resolve cache issues

--- init.lua
+++ init.lua
@@ -16,9 +16,8 @@ end
 -- Gets the commit hash of the HEAD reference for the specified branch
 local function getLatestCommitHash(branch)
     local body = web.get("https://github.com/cwenck/cc-turtles.git/info/refs?service=git-upload-pack")
-    print(body)
     local _, _, hash = string.find(body, "003f(%x+) refs/heads/" .. branch)
-    print("Hash:" .. hash)
+    print("Hash: '" .. hash .. "'")
     return hash
 end
 
@@ -29,13 +28,20 @@ local function downloadProgram(name, commit_hash)
     local url = generateUrlForProgram(name, commit_hash)
     print("URL: " .. url)
 
+    -- Delete tmp file if it exists
+    local tmpFileName = ".tmp-download"
+    fs.delete(tmpFileName)
+
+    -- Download the program
+    web.download(shell, url, tmpFileName)
+
     -- Delete the program if it already exists
     if fs.exists(fullName) then
         fs.delete(fullName)
     end
 
-    -- Download the program
-    web.download(shell, url, fullName)
+    -- Move the downloaded file to the actual file name
+    fs.move("tmpFileName")
 
     print("Finished downloading " .. name)
 end
