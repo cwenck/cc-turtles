179f4231350cc056433d2cab745930a694d68304
Connor Wenck <none@example.com>
2021-01-14 22:13:37 -0500

    Added bootstrap script

---
+++ bootstrap.lua
@@ -0,0 +1,4 @@
+shell.execute("label", "set", "NAME ME!")
+shell.execute("wget", "https://raw.githubusercontent.com/cwenck/cc-turtles/master/init.lua")
+shell.execute("wget", "https://raw.githubusercontent.com/cwenck/cc-turtles/master/web.lua")
+shell.execute("init.lua")
