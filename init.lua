750bf1c92f419d0bfebb1249ca34868c937471ac
Connor Wenck <none@example.com>
2021-01-14 19:39:10 -0500

    Attempt #2 to resolve cache issues

--- init.lua
+++ init.lua
@@ -8,30 +8,21 @@ local programs = {
     "web"
 }
 
-function main()
-    os.loadAPI("web.lua")
-
-    commit_hash = getLatestHeadRef("master")
-    for _, name in ipairs(programs) do
-        downloadProgram(name, commit_hash)
-    end
-end
-
 local function generateUrlForProgram(name, commit_hash)
-    local baseUrl = "https://raw.githubusercontent.com/cwenck/cc-turtles/master/"
+    local baseUrl = "https://raw.githubusercontent.com/cwenck/cc-turtles/master"
     return baseUrl .. "/" .. commit_hash .. "/" .. name .. ".lua"
 end
 
 -- Gets the commit hash of the HEAD reference for the specified branch
-local function getLatestCommitHash(branch) {
-    body = web.get("https://github.com/cwenck/cc-turtles.git/info/refs?service=git-upload-pack")
+local function getLatestCommitHash(branch)
+    local body = web.get("https://github.com/cwenck/cc-turtles.git/info/refs?service=git-upload-pack")
     print(body)
-    _, _, hash = string.find(body, "003f(%x+) refs/heads/" .. branch)
+    local _, _, hash = string.find(body, "003f(%x+) refs/heads/" .. branch)
     print("Hash:" .. hash)
     return hash
-}
+end
 
-function downloadProgram(name, commit_hash)
+local function downloadProgram(name, commit_hash)
     print("Downloading " .. name .. " ...")
     
     local fullName = name .. ".lua"
@@ -47,6 +38,15 @@ function downloadProgram(name, commit_hash)
     web.download(shell, url, fullName)
 
     print("Finished downloading " .. name)
+end
+
+function main()
+    os.loadAPI("web.lua")
+
+    commit_hash = getLatestCommitHash("master")
+    for _, name in ipairs(programs) do
+        downloadProgram(name, commit_hash)
+    end
 end
 
 main()
